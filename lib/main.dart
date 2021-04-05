317ff34f9f0b65cda81028e967323cd9eba5d7fb
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-04-05 03:50:36 +0200

    changing the title of the app from flutter demo to Movie Land

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Movie Land',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
