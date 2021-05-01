cae5c3440f14f31736c86a677783f0b413bbd4cb
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-05-01 23:27:15 +0200

    updating movie repo

--- lib/repositories/movie_repository.dart
+++ lib/repositories/movie_repository.dart
@@ -1,6 +1,9 @@
 import 'package:movieapp2/models/movie.dart';
+import 'package:movieapp2/models/movie_cast.dart';
 import 'package:movieapp2/models/movie_details.dart';
 import 'package:movieapp2/models/movie_trailer.dart';
+import 'package:movieapp2/models/trending.dart';
+import 'package:movieapp2/repositories/trending_repository.dart';
 import 'package:movieapp2/services/movie_service.dart';
 
 class MovieRepository {
@@ -16,5 +19,9 @@ class MovieRepository {
 
   Future<MovieTrailers> fetchMovieTrailers(id) {
     return _movieService.fetchMovieTrailers(id);
+  }
+
+  Future<MovieCast> fetchMovieCast(id) {
+    return _movieService.fetchMovieCast(id);
   }
 }
