3563fea11055a5e5aae85a06fbadc95b8f30efe9
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-05-01 22:48:15 +0200

    Adding Colors Class

---
+++ lib/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorsCollection {
+  const ColorsCollection();
+
+  static const Color mainColor = const Color(0xFF151C26);
+  static const Color secondaryColor = const Color(0xFFa82a2a);
+  static const Color splashTitleColor = const Color(0xff184858);
+  // static const Color mainColor = const Color(0xFF151C26);
+  // static const Color secondColor = const Color(0xFFf4C10F);
+  //  static const Color mainColor = const Color(0xFF151C26);
+  // static const Color secondaryColor = const Color(0xFFa82a2a);
+  // static const Color splashTitleColor = const Color(0xff184858);
+}
