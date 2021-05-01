7b834b294928aa69aa0567f0dadbb2a6db851676
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-05-01 22:55:48 +0200

    updating the Navbar to the google  Navbar

--- lib/Nav.dart
+++ lib/Nav.dart
@@ -1,64 +1,93 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:movieapp2/UI/home_screen/home_screen.dart';
+import 'package:movieapp2/UI/news/news_screen.dart';
+import 'package:movieapp2/colors.dart';
 import 'UI/actors_list/actors_screen.dart';
-import 'UI/favourit_movies/favourti_movies_screen.dart';
+import 'UI/favourit_screen/favourti_movies_screen.dart';
+import 'UI/home_screen/home.dart';
+import 'UI/profile/profile_screen.dart';
 import 'UI/search_movies/search_movies_screen.dart';
 // import 'UI/movie_list/movie_screen.dart';
 
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+
 class Nav extends StatefulWidget {
   @override
-  _NavbarState createState() => _NavbarState();
+  _NavState createState() => _NavState();
 }
 
-class _NavbarState extends State<Nav> {
+class _NavState extends State<Nav> {
   int _selectedIndex = 0;
-  List<Widget> _widgetotpions = <Widget>[
-    Homescreen(),
-    FavMoviesScreen(),
-    SearchScreen(),
-    ActorsScreen(),
-  ];
-
-  void _itemSwitch(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _widgetOptions = <Widget>[
+      Home(),
+      FavMoviesScreen(),
+      SearchScreen(),
+      NewsScreen(),
+      ProfileScreen(),
+    ];
     return Scaffold(
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Color(0xFF151C26),
-        items: <Widget>[
-          Icon(
-            Icons.movie,
-            size: 25,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.favorite,
-            size: 25,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.search,
-            size: 25,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.people,
-            size: 25,
-            color: Colors.white,
-          ),
-        ],
-        onTap: _itemSwitch,
-        height: 45,
-        buttonBackgroundColor: Colors.black,
-        color: Colors.black,
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      body: _widgetotpions.elementAt(_selectedIndex),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: ColorsCollection.mainColor,
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding:
+                const EdgeInsets.only(top: 3, bottom: 3, left: 2, right: 2),
+            child: GNav(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              rippleColor: Colors.grey[300],
+              hoverColor: Colors.grey[100],
+              gap: 8,
+              activeColor: Colors.white,
+              iconSize: 20,
+              haptic: true, // haptic feedback
+              tabBorderRadius: 25,
+
+              curve: Curves.easeOutExpo, // tab animation curves
+              duration: Duration(milliseconds: 400),
+              tabBackgroundColor: ColorsCollection.secondaryColor,
+              color: Colors.white,
+              tabs: [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: LineIcons.heart,
+                  text: 'Saves',
+                ),
+                GButton(
+                  icon: LineIcons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: LineIcons.newspaper,
+                  text: 'News',
+                ),
+                GButton(
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
