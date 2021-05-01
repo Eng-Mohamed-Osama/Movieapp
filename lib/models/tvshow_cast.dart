0a70d548cea0f9955b26b68d3483ae53374ab3c9
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-05-01 22:54:06 +0200

    creating model for TVshow cast

---
+++ lib/models/tvshow_cast.dart
@@ -0,0 +1,98 @@
+// To parse this JSON data, do
+//
+//     final tvshowCast = tvshowCastFromJson(jsonString);
+
+import 'dart:convert';
+
+TvshowCast tvshowCastFromJson(String str) =>
+    TvshowCast.fromJson(json.decode(str));
+
+String tvshowCastToJson(TvshowCast data) => json.encode(data.toJson());
+
+class TvshowCast {
+  TvshowCast({
+    this.cast,
+    this.crew,
+    this.id,
+  });
+
+  List<Cast> cast;
+  List<Cast> crew;
+  int id;
+
+  factory TvshowCast.fromJson(Map<String, dynamic> json) => TvshowCast(
+        cast: List<Cast>.from(json["cast"].map((x) => Cast.fromJson(x))),
+        crew: List<Cast>.from(json["crew"].map((x) => Cast.fromJson(x))),
+        id: json["id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "cast": List<dynamic>.from(cast.map((x) => x.toJson())),
+        "crew": List<dynamic>.from(crew.map((x) => x.toJson())),
+        "id": id,
+      };
+}
+
+class Cast {
+  Cast({
+    this.adult,
+    this.gender,
+    this.id,
+    this.knownForDepartment,
+    this.name,
+    this.originalName,
+    this.popularity,
+    this.profilePath,
+    this.character,
+    this.creditId,
+    this.order,
+    this.department,
+    this.job,
+  });
+
+  bool adult;
+  int gender;
+  int id;
+  String knownForDepartment;
+  String name;
+  String originalName;
+  double popularity;
+  String profilePath;
+  String character;
+  String creditId;
+  int order;
+  String department;
+  String job;
+
+  factory Cast.fromJson(Map<String, dynamic> json) => Cast(
+        adult: json["adult"],
+        gender: json["gender"],
+        id: json["id"],
+        knownForDepartment: json["known_for_department"],
+        name: json["name"],
+        originalName: json["original_name"],
+        popularity: json["popularity"].toDouble(),
+        profilePath: json["profile_path"] == null ? null : json["profile_path"],
+        character: json["character"] == null ? null : json["character"],
+        creditId: json["credit_id"],
+        order: json["order"] == null ? null : json["order"],
+        department: json["department"] == null ? null : json["department"],
+        job: json["job"] == null ? null : json["job"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "adult": adult,
+        "gender": gender,
+        "id": id,
+        "known_for_department": knownForDepartment,
+        "name": name,
+        "original_name": originalName,
+        "popularity": popularity,
+        "profile_path": profilePath == null ? null : profilePath,
+        "character": character == null ? null : character,
+        "credit_id": creditId,
+        "order": order == null ? null : order,
+        "department": department == null ? null : department,
+        "job": job == null ? null : job,
+      };
+}
