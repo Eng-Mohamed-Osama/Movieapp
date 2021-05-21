fac068aace9b2967ed692f1c250d4edf4c259a8f
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-05-21 09:32:27 +0200

    changing the navbar style

--- lib/Nav.dart
+++ lib/Nav.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:movieapp2/UI/news/news_screen.dart';
 import 'package:movieapp2/colors.dart';
-import 'UI/actors_list/actors_screen.dart';
+import 'package:movieapp2/provider/appProvider.dart';
+import 'package:provider/provider.dart';
 import 'UI/favourit_screen/favourti_movies_screen.dart';
 import 'UI/home_screen/home.dart';
 import 'UI/profile/profile_screen.dart';
 import 'UI/search_movies/search_movies_screen.dart';
-// import 'UI/movie_list/movie_screen.dart';
-
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 
 class Nav extends StatefulWidget {
+  final String userId;
+  Nav({this.userId});
   @override
   _NavState createState() => _NavState();
 }
 
 class _NavState extends State<Nav> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
 
   @override
   Widget build(BuildContext context) {
@@ -27,67 +26,69 @@ class _NavState extends State<Nav> {
       Home(),
       FavMoviesScreen(),
       SearchScreen(),
-      NewsScreen(),
+      NewsScreen(userid: widget.userId),
       ProfileScreen(),
     ];
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: ColorsCollection.mainColor,
+    return Consumer<AppProvider>(builder: (buildContext, moviesProvider, _) {
+      return Scaffold(
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
         ),
-        child: SafeArea(
-          child: Padding(
-            padding:
-                const EdgeInsets.only(top: 3, bottom: 3, left: 2, right: 2),
-            child: GNav(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-              rippleColor: Colors.grey[300],
-              hoverColor: Colors.grey[100],
-              gap: 8,
-              activeColor: Colors.white,
-              iconSize: 20,
-              haptic: true, // haptic feedback
-              tabBorderRadius: 25,
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: ColorsCollection.mainColor,
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(top: 3, bottom: 3, left: 2, right: 2),
+              child: GNav(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                rippleColor: Colors.grey[300],
+                hoverColor: Colors.grey[100],
+                gap: 8,
+                activeColor: Colors.white,
+                iconSize: 20,
+                haptic: true, // haptic feedback
+                tabBorderRadius: 25,
 
-              curve: Curves.easeOutExpo, // tab animation curves
-              duration: Duration(milliseconds: 400),
-              tabBackgroundColor: ColorsCollection.secondaryColor,
-              color: Colors.white,
-              tabs: [
-                GButton(
-                  icon: LineIcons.home,
-                  text: 'Home',
-                ),
-                GButton(
-                  icon: LineIcons.heart,
-                  text: 'Saves',
-                ),
-                GButton(
-                  icon: LineIcons.search,
-                  text: 'Search',
-                ),
-                GButton(
-                  icon: LineIcons.newspaper,
-                  text: 'News',
-                ),
-                GButton(
-                  icon: LineIcons.user,
-                  text: 'Profile',
-                ),
-              ],
-              selectedIndex: _selectedIndex,
-              onTabChange: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
+                curve: Curves.easeOutExpo, // tab animation curves
+                duration: Duration(milliseconds: 400),
+                tabBackgroundColor: ColorsCollection.secondaryColor,
+                color: Colors.white,
+                tabs: [
+                  GButton(
+                    icon: LineIcons.home,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: LineIcons.heart,
+                    text: 'Saves',
+                  ),
+                  GButton(
+                    icon: LineIcons.search,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: LineIcons.newspaper,
+                    text: 'News',
+                  ),
+                  GButton(
+                    icon: LineIcons.user,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
