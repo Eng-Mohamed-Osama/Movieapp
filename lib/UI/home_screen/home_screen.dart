bcf8af0f5f17a7a8a67fc23f8379e9fdd50dd4f3
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-05-21 09:32:58 +0200

    update home screen layout

--- lib/UI/home_screen/home_screen.dart
+++ lib/UI/home_screen/home_screen.dart
@@ -1,23 +1,25 @@
 import 'package:badges/badges.dart';
-import 'package:movieapp2/UI/favourit_movies/favourti_movies_screen.dart';
-import 'package:movieapp2/UI/movie_list/movie_list_provider.dart';
+import 'package:movieapp2/UI/favourit_screen/favourti_movies_screen.dart';
 import 'package:movieapp2/UI/movie_list/movie_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:movieapp2/UI/tvShow_list/tvshow_screen.dart';
+import 'package:movieapp2/colors.dart';
+import 'package:movieapp2/provider/appProvider.dart';
 import 'package:provider/provider.dart';
 
-class Homescreen extends StatefulWidget {
-  Homescreen({Key key, this.title}) : super(key: key);
+class HomeMovieTvshowscreen extends StatefulWidget {
+  HomeMovieTvshowscreen({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _HomescreenState createState() => _HomescreenState();
+  _HomeMovieTvshowscreenState createState() => _HomeMovieTvshowscreenState();
 }
 
-class _HomescreenState extends State<Homescreen> {
+class _HomeMovieTvshowscreenState extends State<HomeMovieTvshowscreen> {
   int _selectedIndex = 0;
   List<Widget> _widgetotpions = <Widget>[
     MoviesScreen(),
-    MoviesScreen(),
+    TvshowScreen(),
   ];
   void _itemSwitch(int index) {
     setState(() {
@@ -29,13 +31,16 @@ class _HomescreenState extends State<Homescreen> {
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 2,
-        child: Consumer<MoviesProvider>(
-            builder: (buildContext, moviesProvider, _) {
+        child:
+            Consumer<AppProvider>(builder: (buildContext, moviesProvider, _) {
           var row = Row(
             children: [
               Text(
-                'MovieLand',
-                style: TextStyle(fontStyle: FontStyle.italic),
+                'Movies & TV shows',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  color: ColorsCollection.secondaryColor,
+                ),
               ),
               Padding(
                   padding: const EdgeInsets.only(left: 25),
@@ -43,31 +48,37 @@ class _HomescreenState extends State<Homescreen> {
                     child: Badge(
                       animationType: BadgeAnimationType.scale,
                       shape: BadgeShape.circle,
-                      badgeColor: Colors.red,
+                      badgeColor: ColorsCollection.secondaryColor,
                       child: Icon(
                         Icons.favorite,
                         size: 25,
                       ),
                       badgeContent: Text(
-                          '${MoviesProvider.favMovieCount.toString()}',
+                          '${moviesProvider.favMovieCount.toString()}',
                           style: TextStyle(color: Colors.white, fontSize: 12)),
                     ),
                     onTap: () => {
                       Navigator.of(context).push(MaterialPageRoute(
                           builder: (context) => FavMoviesScreen(
-                              movies: moviesProvider.movies
-                                  .where((m) => m.fav)
-                                  .toList())))
+                                movies: moviesProvider.movies
+                                    .where((m) => m.fav)
+                                    .toList(),
+                                shows: moviesProvider.shows
+                                    .where((m) => m.fav)
+                                    .toList(),
+                              )))
                     },
                   ))
             ],
           );
           return Scaffold(
-              backgroundColor: Color(0xFF151C26),
               appBar: AppBar(
                 title: row,
-                backgroundColor: Colors.black,
+                backgroundColor: ColorsCollection.mainColor,
                 bottom: TabBar(
+                  indicatorColor: ColorsCollection.secondaryColor,
+                  labelColor: ColorsCollection.secondaryColor,
+                  unselectedLabelColor: Colors.grey,
                   tabs: <Widget>[
                     Tab(
                       text: 'Movies',
